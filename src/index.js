fdb521858214af41876ea6857d373a67355653a8
dolcetriade <vcelestialragev@gmail.com>
2019-11-21 00:44:09 -0800

    add a bunch of stuff

--- src/index.js
+++ src/index.js
@@ -1,12 +1,74 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom'
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Lobby from './components/lobby';
+import Game from './components/game';
+import { SnackbarProvider } from 'notistack';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Decrypto
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const useStyles = makeStyles(theme => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <SnackbarProvider maxSnack={3}>
+        <main>
+          <Router>
+            <Route exact path="/" component={Lobby} />
+            <Route path="/game" component={Game} />
+          </Router>
+        </main>
+    </SnackbarProvider>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
